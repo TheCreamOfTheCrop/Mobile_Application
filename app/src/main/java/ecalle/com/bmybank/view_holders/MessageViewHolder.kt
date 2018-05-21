0369ef5397ebf2eb6b5b379ad2b04c263a84ddd6
ThomasEcalle <thomasecalle@hotmail.fr>
2018-05-21 22:32:26 +0200

    added date on chats

--- app/src/main/java/ecalle/com/bmybank/view_holders/MessageViewHolder.kt
+++ app/src/main/java/ecalle/com/bmybank/view_holders/MessageViewHolder.kt
@@ -9,6 +9,8 @@ import ecalle.com.bmybank.R
 import ecalle.com.bmybank.firebase.bo.Message
 import ecalle.com.bmybank.realm.bo.User
 import org.jetbrains.anko.find
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * Created by Thomas Ecalle on 07/04/2018.
@@ -20,25 +22,40 @@ class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     {
         val otherUserImage: ImageView = itemView.find(R.id.otherUserImage)
         val otherUserText: TextView = itemView.find(R.id.otherUsertext)
+        val otherDate: TextView = itemView.find(R.id.otherDate)
+
 
         val userImage: ImageView = itemView.find(R.id.userImage)
         val userText: TextView = itemView.find(R.id.userText)
+        val date: TextView = itemView.find(R.id.date)
+
 
 
         val currentUserLayout: LinearLayout = itemView.find(R.id.currentUserLayout)
         val otherUserLayout: LinearLayout = itemView.find(R.id.otherUserLayout)
+
+
+        val cal = Calendar.getInstance()
+        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.FRANCE)
+        cal.time = sdf.parse(message.date)
+
+        val dateString = "Le ${cal.get(Calendar.DAY_OF_MONTH)}/${cal.get(Calendar.MONTH)}/${cal.get(Calendar.YEAR)} " +
+                "à ${cal.get(Calendar.HOUR_OF_DAY)}h${cal.get(Calendar.MINUTE)}"
+
 
         if (message.id_sender == currentUser.id)
         {
             currentUserLayout.visibility = View.VISIBLE
             otherUserLayout.visibility = View.GONE
             userText.text = message.text
+            date.text = dateString
         }
         else
         {
             otherUserLayout.visibility = View.VISIBLE
             currentUserLayout.visibility = View.GONE
             otherUserText.text = message.text
+            otherDate.text = dateString
         }
     }
 }
